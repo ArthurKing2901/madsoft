a3cdfb4a6a9e5748f51a70db88ebb1e6bef7a46b
admin <admin@admin>
2024-04-16 20:02:36 +0300

    Add react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,71 @@
 import React from 'react'
-import { Button } from '@mui/material'
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material'
 import { useAppDispatch, useAppSelector } from './store/configStore'
 import { getUser } from './store/selectors/userSelector'
 import { Stepper } from './components/Stepper'
 import { getActiveStep } from './store/selectors/stepperSelector'
 import { setActiveStep } from './store/reducers/stepperReducer'
+import { questions } from './mokData'
+import { FormProvider, useForm } from 'react-hook-form'
 
 export const App = () => {
   const user = useAppSelector(getUser)
   const activeStep = useAppSelector(getActiveStep)
   const dispatch = useAppDispatch()
+  const methods = useForm()
+
+  const { handleSubmit } = methods
+  const currentQuestion = questions[activeStep]
 
   const handleSetUser = () => {
     dispatch(setActiveStep({ activeStep: activeStep + 1 }))
   }
 
-  console.log(user)
-
   return (
-    <div className="App">
-      <Stepper steps={['Step 1', 'Step 2', 'Step 3']} activeStep={activeStep} />
-      <Button variant="contained" onClick={handleSetUser}>
-        Ответить
-      </Button>
-    </div>
+    <Container fixed>
+      <FormProvider {...methods}>
+        <Grid container spacing={2} direction="column" alignItems="center">
+          <Grid item>
+            <Stepper
+              steps={['Step 1', 'Step 2', 'Step 3']}
+              activeStep={activeStep}
+            />
+          </Grid>
+          <Grid item>
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {currentQuestion.question}
+                </Typography>
+
+                {currentQuestion.answers.map((answer) => (
+                  <Typography
+                    key={answer}
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    {answer}
+                  </Typography>
+                ))}
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item>
+            <Button variant="contained" onClick={handleSubmit(handleSetUser)}>
+              Ответить
+            </Button>
+          </Grid>
+        </Grid>
+      </FormProvider>
+    </Container>
   )
 }
---
+++ src/mokData.ts
@@ -0,0 +1,28 @@
+export type QuestionType = 'CHECKBOX' | 'RADIO' | 'TEXT'
+export type Question = {
+  id: number
+  type: QuestionType
+  question: string
+  answers: string[]
+}
+
+export const questions: Question[] = [
+  {
+    type: 'CHECKBOX',
+    id: 1,
+    question: 'Вопрос 1',
+    answers: ['Вариант 1', 'Вариант 2', 'Вариант 3'],
+  },
+  {
+    type: 'RADIO',
+    id: 2,
+    question: 'Вопрос 2',
+    answers: ['Вариант 1', 'Вариант 2', 'Вариант 3'],
+  },
+  {
+    type: 'TEXT',
+    id: 3,
+    question: 'Вопрос 3',
+    answers: ['Вариант 1', 'Вариант 2', 'Вариант 3'],
+  },
+]
